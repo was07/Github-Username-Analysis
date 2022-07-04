899def047f0632a280357024fce80e87039d2d81
Was <93242673+was07@users.noreply.github.com>
2022-07-04 22:30:26 +0600

    styling, Followers-following, gists
    yeah

--- index.js
+++ index.js
@@ -18,26 +18,60 @@ function pro(res) {
     document.getElementById('twitter').innerHTML = (found) ? '' : '@' + res.twitter_username;
     document.getElementById('twitter').href = (found) ? '' : 'https://twitter.com/' + res.twitter_username;
 
-    document.getElementById('followers').innerHTML = (found) ? '' : "Followers: " + res.followers;
+    document.getElementById('followers').innerHTML = (found) ? '?' : res.followers.toLocaleString("en-US");
 
-    document.getElementById('following').innerHTML = (found) ? '' : "Following: " + res.following;
+    document.getElementById('following').innerHTML = (found) ? '?' : res.following.toLocaleString("en-US");
 
-    document.getElementById('public_repos').innerHTML = (found) ? '' : "Public Repositories: " + res.public_repos;
+    // get how many followers the user follow
+    get(res.followers_url + "?per_page=100").then(followers => function(followers) {
+        get("https://api.github.com/users/" + res.login + "/following?per_page=100").then(following => proccessFollowers(followers, following, found));
+    }(followers));
+
+    document.getElementById('public_repos').innerHTML = "Public Repositories: " + ((found) ? '?' : res.public_repos);
+    
+    document.getElementById('public_gists').innerHTML = "Public Gists: " + ((found) ? '?' : res.public_gists);
 
     // document.getElementById('stared-topics').innerHTML = (found) ? '' : res.;
+}
+
+function proccessFollowers(followers, following, found) {
+    // return how many followers the user follow
+    console.log(followers)
+    console.log(following)
+    ff = 0;
+    for (follower in followers) {
+        for (followin in following) {
+            if (followers[follower].login == following[followin].login) {
+                ff++;
+            }
+        }
+    }
+    document.getElementById('ff').innerHTML = (found)? '?' : ff.toLocaleString("en-US");
+}
+
+function get(url) {
+    return fetch(url, {
+        method: 'GET',
+        headers: new Headers(
+            {
+                'User-Agent': 'request',
+                'Authorization': 'token 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',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        )
+    }).then(res => res.json());
 }
 
 function fun() {
     u = document.getElementById('input').value;
 
-    res = fetch('https://api.github.com/users/' + u)
-    .then(res => res.json()).then(res => pro(res));
+    get("https://api.github.com/users/" + u).then(res => pro(res));
 }
 
 fun()
 
 // add event listener for enter key for fun()
-document.getElementById('input').addEventListener('keypress', function(e) {
+document.getElementById('input').addEventListener('keyup', function(e) {
     if (e.keyCode == 13) {
         fun();
     }
